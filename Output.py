3f45279f5f271ea304ee53288398a558064c0729
Anurag519Narula <97605849+Anurag519Narula@users.noreply.github.com>
2022-01-25 23:04:09 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Anurag Narula/AppData/Local/Programs/Python/Python310/asg2_21103090.py
+(a)The length of the string is  35
+(b)The reversed string is  egaugnal evitisnes esac a si nohtyP
+(e)The index of substring a is  10
+(f)The original string after removing whitespace is Pythonisacasesensitivelanguage
+
+
+Enter your Name Anurag Narula
+Enter your SID 21103090
+Enter your Department CSE
+Enter your CGPA 9.5
+Hey Anurag Narula, Here!
+My SID is 21103090
+I am from CSE and my CGPA is 9.500000
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Enter first number- 4
+Enter second number- 5
+Enter third number- 2
+The largest number is 5
+
+
+Enter the string-name
+Yes
+
+
+Enter the first side of a triangle- 5
+Enter the second side of a triangle- 4
+Enter the third side of a triangle- 3
+Yes
