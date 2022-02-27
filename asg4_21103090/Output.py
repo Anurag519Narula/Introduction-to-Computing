92deb53daf7ae8d886c7771a1be0ca2d45c4d857
Anurag519Narula <97605849+Anurag519Narula@users.noreply.github.com>
2022-02-27 13:04:26 +0530

    Add files via upload

---
+++ asg4_21103090/Output.py
@@ -0,0 +1,64 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: D:/SUBJECTS/COMPUTING/ASSIGNMENTS/asg4_21103090/Input.py ======
+Soln 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+Soln 2
+Enter the size of pascals triangle
+ 7
+USING LOOPS
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+
+
+Using recursions
+             1  
+           1  1  
+         1  2  1  
+       1  3  3  1  
+     1  4  6  4  1  
+   1  5  10  10  5  1  
+ 1  6  15  20  15  6  1  
+
+
+Soln 3
+Enter 2 numbers: 15 5
+<a> Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The reminder is zero
+<c> Filtered out numbers that are greater than 4 are : [7, 5, 8, 5, 9, 6]
+<d> Set: {5, 6, 7, 8, 9}
+<e> Immutable set: frozenset({5, 6, 7, 8, 9})
+<f> Hash value of the max value from the above set: 9
+
+
+Soln 4 
+Object created
+The name of the student is Anurag and SID is 21103090.
+Object destroyed
+
+
+Soln 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+Soln 6
+Enter the word: Study
+Enter a new meaningful word using the exact same letters to test your friendship: Dusty
+Does the word makes sense?(y/Y(yes) or n/N(no)): Y
+The friends pass the friendship test
